987323e835ed6b8d8549521c9425b25be9e42b34
Etienne <etienne.couzon@gmail.com>
2021-03-12 12:17:11 +0100

    Ajouter/modifierCategorie

---
+++ app/src/main/java/com/td3/gestionbudget/AjouterCatégorie.kt
@@ -0,0 +1,11 @@
+package com.td3.gestionbudget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AjouterCatégorie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ajoutercategorie)
+    }
+}
---
+++ app/src/main/java/com/td3/gestionbudget/ModifierCatégorie.kt
@@ -0,0 +1,12 @@
+package com.td3.gestionbudget
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+import com.td3.gestionbudget.R
+
+class ModifierCatégorie : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.modifiercategorie)
+    }
+}
