9974f43be7479fe5dd446fef56ef9240f33affed
LeoThinnes <63150113+LeoThinnes@users.noreply.github.com>
2021-03-12 12:04:47 +0100

    ajout nouveau revenu

--- app/src/main/java/com/td3/gestionbudget/MainActivity.kt
+++ app/src/main/java/com/td3/gestionbudget/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.td3.gestionbudget
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
 }
